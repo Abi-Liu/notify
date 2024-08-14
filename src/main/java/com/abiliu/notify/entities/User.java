0a3136a5cfe24c843743a7f99772adae00dc0a9f
Abi Liu <abiliu018@gmail.com>
2024-08-14 10:42:20 -0700

    user entity

---
+++ src/main/java/com/abiliu/notify/entities/User.java
@@ -0,0 +1,33 @@
+package com.abiliu.notify.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+}
